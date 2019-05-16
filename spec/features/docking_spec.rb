e13192ceb989baf9fc58ef3588640336172b09bb
Anthony Hull <anthony.hull@hotmail.co.uk>
2019-05-16 15:51:54 +0100

    added another mixed bike feature test Tests failiure state where the station runs out of working bikes

--- spec/features/docking_spec.rb
+++ spec/features/docking_spec.rb
@@ -28,5 +28,13 @@ describe 'docking_station.add_bike.release_bike' do
       2.times{@docking_station.release_bike}
       expect{@docking_station.release_bike}.to_not raise_error
     end
+    it '4.times.release_bike.working?' do
+      @bike = Bike.new
+      @docking_station = DockingStation.new
+      3.times{@bike = Bike.new; @docking_station.add_bike(@bike)}
+      6.times{@bike = Bike.new; @bike.broken; @docking_station.add_bike(@bike)}
+      3.times{@docking_station.release_bike}
+      expect{@docking_station.release_bike}.to raise_error('No bikes are available')
+    end
   end
 end
