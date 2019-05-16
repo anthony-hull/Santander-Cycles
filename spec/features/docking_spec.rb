38652268a4693b9a96e0c6b1c48442beccd8c8ef
Anthony Hull <anthony.hull@hotmail.co.uk>
2019-05-16 14:11:22 +0100

    test for Getting broken bike out of docking station.

--- spec/features/docking_spec.rb
+++ spec/features/docking_spec.rb
@@ -2,10 +2,19 @@ require 'bike'
 require 'docking_station'
 
 describe 'docking_station.add_bike.release_bike' do
-  it '.working?' do
-    bike = Bike.new
-    docking_station = DockingStation.new
-    docking_station.add_bike(bike)
-    expect(docking_station.release_bike.working?).to eql(true)
+  before(:each) do
+    @bike = Bike.new
+    @docking_station = DockingStation.new
+  end
+  it '.release_bike.working?' do
+    @docking_station.add_bike(@bike)
+    expect(@docking_station.release_bike.working?).to eql(true)
+  end
+  context 'DockingStation:Bikes = [Bike:working = false]' do
+    it 'release_bike.working?' do
+      @bike.broken
+      @docking_station.add_bike(@bike)
+      expect(@docking_station.release_bike.working?).to eql(false)
+    end
   end
 end
